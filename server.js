6e381c81d33e4c46b30a7354b208ea6515780f87
LaMonica_Arnold <ltarnold03@yahoo.com>
2021-07-04 22:58:45 -0500

    soft code port

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const express = require('express');
-
+const fs = require('fs');
 const app = express();
 
-app.listen(3000, () => {
-    console.log(`API server now on port 3000!`);
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`API server now on ${PORT}!`);
 });
